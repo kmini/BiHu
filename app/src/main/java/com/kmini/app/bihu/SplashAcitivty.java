63aab0750a6e91dace387c0dede840819cb63cc7
hukai <hukai0663@qq.com>
2017-04-15 17:31:51 +0800

    第一版

---
+++ app/src/main/java/com/kmini/app/bihu/SplashAcitivty.java
@@ -0,0 +1,17 @@
+package com.kmini.app.bihu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by hukai on 2017/4/7.
+ */
+
+public class SplashAcitivty extends Activity {
+
+    @Override
+    protected void onCreate (Bundle saveInstanceState)
+    {
+
+    }
+}
